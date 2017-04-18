f10ff7542738f86f4c842f86d13cf9c27a2e7510
kouki <kouki@10.70.85.65>
2017-04-18 16:06:25 +0900

    make koma class

--- src/shogi/Koma.java
+++ src/shogi/Koma.java
@@ -1,5 +1,47 @@
 package shogi;
 
 public class Koma {
+	private boolean ev = false;//成
+	private boolean enemy = false;//敵味方
+	private int pos_x;
+	private int pos_y;
+	enum komainf{
+		FU,
+		KY,
+		KE,
+		GI,
+		KI,
+		KA,
+		HI,
+		OU,
+	}
+	komainf koma;
+
+	Koma(komainf k,int x,int y)
+	{
+		koma = k;
+		pos_x = x;
+		pos_y = y;
+	}
+
+	String name[] = {
+			"歩","香","桂","銀","角","飛","金","玉",
+			"と","杏","圭","全","馬","竜",
+	};
+
+	boolean isSelf()
+	{
+		return enemy;
+	}
+
+	@Override
+	public String toString()
+	{
+		if(ev)
+		{
+			return name[koma.ordinal()+8];
+		}
+		return name[koma.ordinal()];
+	}
 
 }
